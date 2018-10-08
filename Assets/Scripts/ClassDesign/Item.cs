a37512e93b741aef124a3bbef41b29b80a735a1e
nature <2726916340@qq.com>
2018-10-08 17:26:41 +0800

    2018.10.8 Start the project of dragon knight

---
+++ Assets/Scripts/ClassDesign/Item.cs
@@ -0,0 +1,56 @@
+﻿//The Item class for items
+//By nature
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item{
+	private string name;		//The Item name
+	private string price;		//The price of the Item, 50% off for sale
+	private string description;	//The description of the item
+	private int take_place;		//How many the item would take the place of packages
+	#region  attributes accessor
+	public string Name
+	{
+		get
+		{
+			return name;
+		}
+	}
+	public string Price
+	{
+		get
+		{
+			return price;
+		}
+	}
+	public string Description
+	{
+		get
+		{
+			return description;
+		}
+	}
+	public int Take_place
+	{
+		get
+		{
+			return take_place;
+		}
+	}
+	#endregion
+	///<summary>Constructor</summary>
+	///<param name="_name">The Item name</param>
+	///<param name="_price">The price of the Item, 50% off for sale</param>
+	///<param name="_description">The description of the item</param>
+	///<param name="_take_place">How many the item would take the place of packages	</param>
+	public Item(string _name,string _price,string _description,int _take_place)
+	{
+		name = _name;
+		price = _price;
+		description = _description;
+		take_place = _take_place;
+	}
+
+	public virtual void UseItem(Role user){}
+}
---
+++ Assets/Scripts/ClassDesign/Role.cs
@@ -0,0 +1,381 @@
+﻿//This class is designed for the all role in the game
+//By nature 2018.10.8
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Role{
+	private string name;				//role name
+	private int gender;					//4 kinds of sex
+	private string race;				//Different race
+	private int level;					//The Level of the Role , for 7 levels
+
+	private int force_attack;			//The force attack
+	private int force_defense;			//The force defense
+	private int force_hit;				//The force hit
+	private float force_critical;		//The max num of Critical force hit
+	private int force_probability;		//The propability of Critical force hit
+	
+	private int magic_attack;			//The magic attack
+	private int magic_defense;			//The magic defense
+	private int magic_hit;				//The magic hit
+	private float magic_critical;		//The max num of Critical magic hit
+	private int magic_probability;		//The probability of Critical magic hit
+
+	private int health;					//The max num of life
+	private int magic;					//The max num of magic
+	private int move_speed;				//The move speed
+	private int dodge;					//The dodge
+	private int current_health;			//The current num of life
+	private int current_magic;			//The current num of magic
+
+#region  attributes accessors
+	public string Name
+	{
+		get
+		{
+			return name;
+		}
+	}
+	public string Gender
+	{
+		get
+		{
+			switch (gender)
+			{
+				case 0:
+				{
+					return "Male";
+				}
+				case 1:
+				{
+					return "Female";
+				}
+				case 2:
+				{
+					return "No gender";
+				}
+				case 3:
+				{
+					return "Monoecious";
+				}
+				default:
+				{
+					return "Unknow";
+				}
+			}
+		}
+	}
+	public string Race
+	{
+		get
+		{
+			return race;
+		}
+	}
+	public string Level
+	{
+		get
+		{
+			switch(level)
+			{
+				case 0:
+				{
+					return "菜鸟级";
+				}
+				case 1:
+				{
+					return "新手级";
+				}
+				case 2:
+				{
+					return "冒险家";
+				}
+				case 3:
+				{
+					return "大师级";
+				}
+				case 4:
+				{
+					return "传奇级";
+				}
+				case 5:
+				{
+					return "圣域级";
+				}
+				case 6:
+				{
+					return "星空级";
+				}
+				case 7:
+				{
+					return "大帝";
+				}
+				default:
+				{
+					return "Level Error";
+				}
+			}
+		}
+	}
+	public int Force_attack
+	{
+		get
+		{
+			return force_attack;
+		}
+	}
+	public int Force_defense
+	{
+		get
+		{
+			return force_defense;
+		}
+	}
+	public int Force_hit
+	{
+		get
+		{
+			return force_hit;
+		}
+	}
+	public float Force_critical
+	{
+		get
+		{
+			return force_critical;
+		}
+	}
+	public int Force_probability
+	{
+		get
+		{
+			return force_probability;
+		}
+	}
+	public int Magic_attack
+	{
+		get
+		{
+			return magic_attack;
+		}
+	}
+	public int Magic_defense
+	{
+		get
+		{
+			return magic_defense;
+		}
+	}
+	public int Magic_hit
+	{
+		get
+		{
+			return magic_hit;
+		}
+	}
+	public float Magic_critical
+	{
+		get
+		{
+			return magic_critical;
+		}
+	}
+	public int Magic_probability
+	{
+		get
+		{
+			return magic_probability;
+		}
+	}
+	public int Health
+	{
+		get
+		{
+			return health;
+		}
+	}
+	public int Magic
+	{
+		get
+		{
+			return magic;
+		}
+	}
+	public int Move_speed
+	{
+		get
+		{
+			return move_speed;
+		}
+	}
+	public int Dodge
+	{
+		get
+		{
+			return dodge;
+		}
+	}
+	public int Current_health
+	{
+		set
+		{
+			if(value <= 0)
+			{
+				//The role Dead
+				current_health = 0;
+				Dead();
+			}
+			else if(value >= health)
+			{
+				//The max health
+				current_health = health;
+			}
+			else
+			{
+				current_health = value;
+			}
+		}
+		get
+		{
+			return current_health;
+		}
+	}
+	public int Current_magic
+	{
+		set
+		{
+			if(value <= 0)
+			{
+				current_magic = 0;
+			}
+			else if(value >= Magic)
+			{
+				current_magic = Magic;
+			}
+			else
+			{
+				current_magic = value;
+			}
+		}
+		get
+		{
+			return current_magic;
+		}
+	}
+#endregion
+
+	///<summary>The Default Constructor of Role Class</summary>
+	///<param name="_name">name</param>
+	///<param name="_sex">sex</param>
+	///<param name="_race">race</param>
+	///<param name="_level">level</param>
+	///<param name="_force_attack">The force attack</param>
+	///<param name="_force_defense">The force defense</param>
+	///<param name="_force_hit">The force hit</param>
+	///<param name="_force_critical">The max num of Critical force hit</param>
+	///<param name="_force_probability">The propability of Critical force hit</param>
+	///<param name="_magic_attack">The magic attack</param>
+	///<param name="_magic_defense">The magic defense</param>
+	///<param name="_magic_hit">The magic hit</param>
+	///<param name="_magic_critical">The max num of Critical magic hit</param>
+	///<param name="_magic_probability">The probability of Critical magic hit</param>
+	///<param name="_health">The max num of life</param>
+	///<param name="_magic">The max num of magic</param>
+	///<param name="_move_speed">The move speed</param>
+	///<param name="_dodge">The dodge</param>
+	///<param name="_current_health">The current num of health</param>
+	///<param name="_current_magic">The current num og magic</param>
+	public Role(string _name,int _gender,string _race,int _level,
+				int _force_attack,int _force_defense,int _force_hit,float _force_critical,int _force_probability,
+				int _magic_attack,int _magic_defense,int _magic_hit,float _magic_critical,int _magic_probability,
+				int _health,int _magic,int _move_speed,int _dodge,int _current_health,int _current_magic)
+				{
+					name = _name;
+					gender = _gender;
+					race = _race;
+					level = _level;
+					force_attack = _force_attack;
+					force_defense = _force_defense;
+					force_hit = _force_hit;
+					force_critical = _force_critical;
+					force_probability = _force_probability;
+					magic_attack = _magic_attack;
+					magic_defense = _magic_defense;
+					magic_hit = _magic_hit;
+					magic_critical = _magic_critical;
+					magic_probability = _magic_probability;
+					health = _health;
+					magic = _magic;
+					move_speed = _move_speed;
+					dodge = _dodge;
+					current_health = _current_health;
+					current_magic = _current_magic;
+				}
+	#region  need to be modified soon , because there are many problems
+	///<summary>Attack Enemy in Force</summary>
+	///<param name="enemy">The Attack Enemy</param>
+	public void AttackWithForce(Role enemy)
+	{
+		float tmp = 1.0f;
+		if(CriticalHitHelper(force_probability))
+		{
+			//Critical Hit!
+			tmp = force_critical;
+		}
+		else
+		{
+			tmp = 1.0f;
+		}
+		float damage = force_attack*tmp*(force_hit-enemy.Dodge)/force_hit - enemy.Force_defense;
+		if(damage <= 0)
+		{
+			damage = 1;
+		}
+		enemy.Current_health -= (int)damage;
+	}
+
+	///<summary>Attack Enemy in Magic</summary>
+	///<param name="enemy">The Attack Enemy</param>
+	public void AttackWithMagic(Role enemy)
+	{
+		float tmp = 1.0f;
+		if(CriticalHitHelper(magic_probability))
+		{
+			//Critical Hit!
+			tmp = magic_critical;
+		}
+		else
+		{
+			tmp = 1.0f;
+		}
+		float damage = magic_attack*tmp*(magic_hit-enemy.Dodge)/magic_hit - enemy.Magic_defense;
+		if(damage <= 0)
+		{
+			damage = 1;
+		}
+		enemy.Current_health -= (int)damage;
+	}
+
+	///<summary>Judge whether Critical Hit</summary>
+	///<param name="probability">The ptobility of the attack</param>
+	///<return>true,Critical Hit; False normal Hit</return>
+	private bool CriticalHitHelper(int probability)
+	{
+		int temp = Random.Range(0,100);
+		if(temp < probability)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	public void Dead()
+	{
+		Debug.Log("In RoleCalss.cs : "+Name + " is dead.");
+	}
+	#endregion	
+}
